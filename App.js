4267af24a20d6f4edc7733af880e4a9bddde5596
Victoria Tran <victoriahcntran@gmail.com>
2022-03-06 21:35:08 -0600

    in progress

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import DashboardScreen from './screens/DashboardScreen';
 import ClubScreen from './screens/ClubScreen';
 import SearchScreen from './screens/SearchScreen';
 import FilterScreen from  './screens/FilterScreen';
@@ -15,6 +16,9 @@ const [filter, updateFilter] = useState([]);
     <FilterContext.Provider value={filter}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="DashboardScreen"
+            component={DashboardScreen}
+          />
           <Stack.Screen name="SearchScreen"
             component={SearchScreen}
           />
